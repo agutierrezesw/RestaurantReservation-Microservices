313318b8e7ea95ec634f7eadbe551d02e1acfc56
Marc Zurriaga Mitjans <m.zurriaga.mitjans@avanade.com>
2024-06-06 12:40:09 +0200

    Add project files.

---
+++ UserManagement.Infrastructure/Configuration/CustomerConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserManagement.Domain.Entities.Customers;
+
+namespace UserManagement.Infrastructure.Configuration
+{
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> entity)
+        {
+            entity.HasKey(e => e.Id);
+
+            entity.Property(e => e.FirstName)
+                .IsRequired()
+                .HasMaxLength(250)
+                .IsUnicode(false);
+
+            entity.Property(e => e.LastName)
+                .IsRequired()
+                .HasMaxLength(250)
+                .IsUnicode(false);
+
+            //entity.Property(e => e.TotalNumberOfReservations);
+        }
+    }
+}
---
+++ UserManagement.Infrastructure/Repository/InMemoryCustomerRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserManagement.Domain.Entities.Customers;
+using UserManagement.Domain.Repositories;
+
+namespace UserManagement.Infrastructure.Repository
+{
+    internal class InMemoryCustomerRepository: ICustomerRepository
+    {
+        private static readonly List<Customer> _items = [];
+        public InMemoryCustomerRepository() { }
+
+        Task<Customer?> ICustomerRepository.GetByIdAsync(int id, CancellationToken cancellationToken)
+        {
+            var customer = _items != null ? _items.FirstOrDefault(i => i.Id == id) : null;
+
+            return Task.FromResult(customer);
+        }
+
+        Task<int> ICustomerRepository.AddAndSaveAsync(Customer item, CancellationToken cancellationToken)
+        {
+            int id = _items.Count > 0 ? _items.Max(i => i.Id) + 1 : 1;
+            item.Id = id;
+            _items.Add(item);
+
+            return Task.FromResult(item.Id);
+        }
+
+        Task ICustomerRepository.DeleteByIdAsync(int id, CancellationToken cancellationToken)
+        {
+            _items.RemoveAll(i => i.Id == id);
+
+            return Task.CompletedTask;
+        }
+    }
+}
