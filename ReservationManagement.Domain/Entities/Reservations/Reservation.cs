59be057ffcf13fc8f3c3b019a2bdc82ffa7d58ae
Davide Pizzato <dpizzato@esw.com>
2024-06-11 13:39:17 +0200

    chore: remove unneeded usings

--- ReservationManagement.Domain/Entities/Reservations/Reservation.cs
+++ ReservationManagement.Domain/Entities/Reservations/Reservation.cs
@@ -1,9 +1,4 @@
 ﻿using RestaurantReservation.Core.Domain.Abstractions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ReservationManagement.Domain.Entities.Reservations
 {
--- ReservationManagement.Domain/Repositories/IReservationRepository.cs
+++ ReservationManagement.Domain/Repositories/IReservationRepository.cs
@@ -1,9 +1,4 @@
 ﻿using ReservationManagement.Domain.Entities.Reservations;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ReservationManagement.Domain.Repositories
 {
