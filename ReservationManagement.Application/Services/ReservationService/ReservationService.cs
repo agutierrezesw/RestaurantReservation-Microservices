d4fe996a86160c3355f3ebd03ae9ffc4b3b483a0
Davide Pizzato <dpizzato@esw.com>
2024-06-11 14:02:01 +0200

    feat: add empty reservation service

---
+++ ReservationManagement.Application/Services/ReservationService/ReservationService.cs
@@ -0,0 +1,18 @@
+﻿using ReservationManagement.Domain.Repositories;
+
+namespace ReservationManagement.Application.Services.ReservationServices
+{
+    public class ReservationService
+    {
+        private readonly ICustomerRepository _customerRepository;
+        private readonly IReservationRepository _reservationRepository;
+        private readonly IRestaurantRepository _restaurantRepository;
+
+        public ReservationService(ICustomerRepository customerRepository, IReservationRepository reservationRepository, IRestaurantRepository restaurantRepository)
+        {
+            _customerRepository = customerRepository;
+            _reservationRepository = reservationRepository;
+            _restaurantRepository = restaurantRepository;
+        }
+    }
+}
