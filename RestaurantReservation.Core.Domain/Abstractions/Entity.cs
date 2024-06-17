fec6e1ba2a4d66f88617a3b5ffac4f2c3e0ff4a5
Davide Pizzato <dpizzato@esw.com>
2024-06-17 16:59:24 +0200

    chore: fix some missing include in csproj

--- RestaurantReservation.Core.Domain/Abstractions/Entity.cs
+++ RestaurantReservation.Core.Domain/Abstractions/Entity.cs
@@ -1,22 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace RestaurantReservation.Core.Domain.Abstractions;
 
-namespace RestaurantReservation.Core.Domain.Abstractions
+public abstract class Entity
 {
-    public abstract class Entity
+    protected Entity(int id)
     {
-        protected Entity(int id)
-        {
-            Id = id;
-        }
-
-        protected Entity()
-        {
-        }
-
-        public int Id { get; set; }
+        Id = id;
     }
+
+    protected Entity()
+    {
+    }
+
+    public int Id { get; set; }
 }
--- RestaurantReservation.Core.Domain/Abstractions/IUnitOfWork.cs
+++ RestaurantReservation.Core.Domain/Abstractions/IUnitOfWork.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace RestaurantReservation.Core.Domain.Abstractions;
 
-namespace RestaurantReservation.Core.Domain.Abstractions
+public interface IUnitOfWork
 {
-    public interface IUnitOfWork
-    {
-        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
-    }
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
